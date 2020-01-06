37f25746b9195bb3cf1667059ba7691dda32933e
ram <meerdara@hotmail.com>
2020-01-05 21:24:19 -0500

    fixed server side test with mock user

--- src/test/java/com/nano/store/web/rest/ProductOrderResourceIT.java
+++ src/test/java/com/nano/store/web/rest/ProductOrderResourceIT.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +36,7 @@ import com.nano.store.domain.enumeration.OrderStatus;
  * Integration tests for the {@link ProductOrderResource} REST controller.
  */
 @SpringBootTest(classes = StoreApp.class)
+@WithMockUser(username = "admin", authorities = {"ROLE_ADMIN"}, password = "admin")
 public class ProductOrderResourceIT {
 
     private static final Instant DEFAULT_PLACED_DATE = Instant.ofEpochMilli(0L);
